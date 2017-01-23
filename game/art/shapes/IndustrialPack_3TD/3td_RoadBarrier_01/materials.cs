e874678076f3d9bced79591750bdba2bd89bf1ce
MegaMotion <chris@megamotionsoftware.com>
2017-01-22 22:01:38 -0800

    adding industrial pack back in, for road barriers.

---
+++ game/art/shapes/IndustrialPack_3TD/3td_RoadBarrier_01/materials.cs
@@ -0,0 +1,12 @@
+
+singleton Material(RoadBarrier_01_Concrete01)
+{
+   mapTo = "Concrete01";
+   diffuseMap[0] = "3td_concrete_01.jpg";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "3td_concrete_01_NRM.png";
+   useAnisotropic[0] = "1";
+   doubleSided = "1";
+};
---
+++ game/art/shapes/IndustrialPack_3TD/3td_WeldingTank_01/materials.cs
@@ -0,0 +1,32 @@
+
+singleton Material(WeldingTank01_tank02)
+{
+   mapTo = "tank02";
+   diffuseMap[0] = "3td_Tank_02";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "19";
+   translucentBlendOp = "None";
+   pixelSpecular[0] = "1";
+   useAnisotropic[0] = "1";
+   doubleSided = "1";
+};
+
+singleton Material(WeldingTank01_ColorEffectR8G61B138_material)
+{
+   mapTo = "ColorEffectR8G61B138-material";
+   diffuseColor[0] = "0.0313726 0.239216 0.541176 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(WeldingTank01_tank01)
+{
+   mapTo = "tank01";
+   diffuseMap[0] = "3td_Tank_01.jpg";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "24";
+   translucentBlendOp = "None";
+   pixelSpecular[0] = "1";
+   useAnisotropic[0] = "1";
+   doubleSided = "1";
+};
